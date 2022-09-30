46d4cecfbb788b864ea07a6f7d2fd1ad5be81c14
Jiakuan(Daniel) Li <jiakuan.li.cs@gmail.com>
2022-09-30 16:52:37 +0900

    pytest style

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,96 +1,51 @@
-import unittest
 import uuid
-from unittest.mock import MagicMock, patch
 
+import fastapi
 import pytest
 
 from app.controllers.speak import speak
 from app.controllers.status import get_status
 from app.controllers.voice import _get_voices_from_cache, _set_languages_to_cache
-from app.database.database import db
-from app.database.redis import redis_client
-from app.models import record
 from app.models.speak import SpeakRequest
 from app.models.voice import VoiceInformation
-from app.tts.azure import azure_clint
 
 
-@pytest.mark.usefixtures("docker")
-class TestHandlerSpeak(unittest.TestCase):
-    db_data = {}
+def test_get_status(records):
+    for k, v in records.items():
+        resp = get_status(v.task_id)
+        assert resp.code == 0
+        assert resp.status == v.status
+        assert resp.msg == ""
+        if v.status == "success":
+            assert resp.download_url == v.download_url
+        else:
+            assert resp.download_url is None
 
-    def setUp(self) -> None:
-        redis_client.init(host="localhost", port=6379)
-        db.init_db(
-            db_name="test", host="localhost", port=3306, user="root", password="mysql"
-        )
-        azure_clint.init()
+    resp = get_status(uuid.uuid4().hex)
+    assert resp.code == 404
+    assert resp.status == "unknown"
 
-        super().setUp()
 
-        for i in range(30):
-            task_id = uuid.uuid4()
-            if i % 4 == 0:
-                status = "pending"
-            elif i % 4 == 1:
-                status = "processing"
-            elif i % 4 == 2:
-                status = "success"
-            else:
-                status = "failed"
-            r = record.Record.create(
-                task_id=task_id,
-                service="azure",
-                callback=uuid.uuid4().__str__(),
-                speed="normal",
-                status=status,
-                download_url=uuid.uuid4().__str__(),
-                note="",
-                audio_content=b"",
-            )
-            self.db_data[task_id] = r
+def test_speak_handler(mysql, mocker):
+    request = SpeakRequest(
+        service="azure",
+        text="hello world",
+        language="en-US",
+        voice="en-US-AriaNeural",
+    )
+    mock_background_tasks = mocker.patch.object(fastapi.BackgroundTasks, "add_task")
+    speak(request, mock_background_tasks)
+    mock_background_tasks.add_task.assert_called_once()
 
-    def tearDown(self) -> None:
-        try:
-            db.db.drop_tables([record.Record])
-        except:
-            pass
-        db.close_db()
-        super().tearDown()
 
-    def test_get_status(self):
-        for k, v in self.db_data.items():
-            resp = get_status(v.task_id)
-            self.assertEqual(resp.code, 0)
-            self.assertEqual(resp.status, v.status)
-            self.assertEqual(resp.msg, "")
-            if v.status == "success":
-                self.assertEqual(resp.download_url, v.download_url)
-            else:
-                self.assertEqual(resp.download_url, None)
-        resp = get_status(uuid.uuid4().hex)
-        self.assertEqual(resp.code, 404)
-        self.assertEqual(resp.status, "unknown")
+@pytest.mark.asyncio
+async def test_get_voices_from_redis_hit(redis):
+    voices = {
+        "zh": [VoiceInformation(name="zh", gender="male")],
+        "en": [VoiceInformation(name="en", gender="male")],
+    }
+    await _set_languages_to_cache("azure", "zh", voices["zh"])
+    get = await _get_voices_from_cache("azure", "zh")
 
-    @patch("fastapi.BackgroundTasks")
-    def test_speak_handler(self, mock_background_tasks: MagicMock):
-        request = SpeakRequest(
-            service="azure",
-            text="hello world",
-            language="en-US",
-            voice="en-US-AriaNeural",
-        )
-        with patch.object(mock_background_tasks, "add_task") as mock_add_task:
-            speak(request, mock_background_tasks)
-            mock_add_task.assert_called_once()
-
-    def test_get_voices_from_redis_hit(self):
-        voices = {
-            "zh": [VoiceInformation(name="zh", gender="male")],
-            "en": [VoiceInformation(name="en", gender="male")],
-        }
-        _set_languages_to_cache("azure", "zh", voices["zh"])
-        get = _get_voices_from_cache("azure", "zh")
-
-        assert get[0].name == "zh"
-        assert get[0].gender == "male"
+    assert get[0].name == "zh"
+    assert get[0].gender == "male"
