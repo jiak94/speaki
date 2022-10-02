1cddfcc420501d3118bbd8e95ea2fa6ba7fbadfb
Jiakuan(Daniel) Li <jiakuan.li.cs@gmail.com>
2022-10-02 19:09:12 +0900

    replace requests with httpx

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,12 +1,15 @@
 import os
 import uuid
 
+import pytest
+
 from app import config, tasks
 from app.controllers.speak import _wrap_with_ssml
 from app.models.record import Record
 
 
-def test_speak(mysql, mock_file, azure):
+@pytest.mark.asyncio
+async def test_speak(mysql, mock_file, azure):
     config.ENABLE_EXTERNAL_STORAGE = False
 
     task_id = str(uuid.uuid4())
@@ -18,7 +21,7 @@ def test_speak(mysql, mock_file, azure):
         speed="normal",
     )
     ssml = _wrap_with_ssml("Hello World", "medium", "en-US", "en-US-AriaNeural")
-    tasks.speak(ssml, "azure", task_id)
+    await tasks.speak(ssml, "azure", task_id)
     record = Record.get(task_id=task_id)
     assert record is not None
     assert record.status == "success"
@@ -28,7 +31,8 @@ def test_speak(mysql, mock_file, azure):
     assert os.path.exists(file_path)
 
 
-def test_speak_unknown_service(mysql, mock_file, azure):
+@pytest.mark.asyncio
+async def test_speak_unknown_service(mysql, mock_file, azure):
     task_id = str(uuid.uuid4())
     Record.create(
         task_id=task_id,
@@ -38,7 +42,7 @@ def test_speak_unknown_service(mysql, mock_file, azure):
         speed="normal",
     )
     ssml = _wrap_with_ssml("Hello World", "medium", "en-US", "en-US-AriaNeural")
-    tasks.speak(ssml, "unknown", task_id)
+    await tasks.speak(ssml, "unknown", task_id)
     file_path = os.path.join(config.MEDIA_PATH, f"{task_id}.wav")
     assert not os.path.exists(file_path)
 
@@ -48,11 +52,13 @@ def test_speak_unknown_service(mysql, mock_file, azure):
     assert record.note == "Service not supported"
 
 
-def test_speak_unknow_record(mysql, mock_file, azure):
+@pytest.mark.asyncio
+async def test_speak_unknow_record(mysql, mock_file, azure):
     ssml = _wrap_with_ssml("Hello World", "medium", "en-US", "en-US-AriaNeural")
-    tasks.speak(ssml, "azure", str(uuid.uuid4()))
+    await tasks.speak(ssml, "azure", str(uuid.uuid4()))
 
 
+@pytest.mark.asyncio
 def test_storage_service():
     config.ENABLE_EXTERNAL_STORAGE = True
     config.EXTERNAL_STORAGE_SERVICE = "azure"
