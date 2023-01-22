72d76a1a1874213e609faeed8a824a216625b084
Jiakuan Li <jiakuan.li.cs@gmail.com>
2023-01-22 00:11:07 -0500

    add s3 support

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,4 +1,6 @@
 import os
+import httpx
+import uuid
 
 
 def test_azure_blob(azure_storage_service):
@@ -10,3 +12,22 @@ def test_azure_blob(azure_storage_service):
     azure_storage_service.upload_file(file)
 
     os.remove(file)
+
+def test_aws_s3_upload(aws_storage_service):
+    uid = uuid.uuid4()
+    f = open("./uuid.txt", "w")
+    f.write(uid.__str__())
+    f.close()
+
+    file = os.path.join("./", "uuid.txt")
+    download_url = aws_storage_service.upload_file(file)
+
+    assert download_url is not None
+
+    assert httpx.get(download_url).text == uid.__str__()
+
+    os.remove(file)
+
+def test_aws_s3_create_bucket(aws_storage_service):
+    assert aws_storage_service.create_bucket("test.bucket.jiakuan")
+    aws_storage_service.client.delete_bucket(Bucket="test.bucket.jiakuan")
